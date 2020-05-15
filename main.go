9ab4fa959eec7948300d8e83b9d4051f36fb7e3e
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-05-15 22:29:54 +0530

    added persistance to update route

--- main.go
+++ main.go
@@ -30,9 +30,6 @@ type Parent struct {
 	Lastname  string `json:"lastname"`
 }
 
-// Local datastore save all students inside a slice named students which contain vlues of type Student
-var students []Student
-
 var studentColl *mongo.Collection
 
 // Get all Students
@@ -80,13 +77,10 @@ func createStudent(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&student)
 	student.Roll = strconv.Itoa(rand.Intn(1000000))
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-
 	insertResult, err := studentColl.InsertOne(ctx, student)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// students = append(students, student)
 	json.NewEncoder(w).Encode(insertResult)
 }
 
@@ -95,16 +89,18 @@ func updateStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // Get params
 	var student Student
-	_ = json.NewDecoder(r.Body).Decode(&student)
-	for index, item := range students {
-		if item.Roll == params["roll"] {
-			student.Roll = params["roll"] // Roll is internal and cannot be changed
-			students = append(append(students[:index], student), students[index+1:]...)
-			log.Println(item)
-			break
-		}
+	json.NewDecoder(r.Body).Decode(&student)
+	student.Roll = params["roll"] // Roll is internal and cannot be changed
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	_, err := studentColl.DeleteMany(ctx, bson.M{"roll": params["roll"]})
+	if err != nil {
+		log.Fatal(err)
 	}
-	json.NewEncoder(w).Encode(students)
+	insertResult, err := studentColl.InsertOne(ctx, student)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(insertResult)
 }
 
 // Delete a Student
@@ -117,6 +113,7 @@ func deleteStudent(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	json.NewEncoder(w).Encode(deleteResult)
+	// Handle error when wrong roll number provided
 }
 
 func main() {
@@ -130,27 +127,9 @@ func main() {
 	}
 	fmt.Println("Connected to MongoDB!")
 	studentColl = client.Database("test").Collection("students")
-	// fmt.Printf("type of ctx variable = %T\n", ctx)
-
-	// insertResult, err := studentColl.InsertOne(ctx, Student{Roll: "1", Branch: "123456", Name: "Student 1",
-	// 	Parent: &Parent{Firstname: "saurabh", Lastname: "raj"}})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("Inserted a single document: ", insertResult.InsertedID)
 
 	// Init router
 	r := mux.NewRouter()
-
-	students = append(students, Student{Roll: "1", Branch: "123456", Name: "Student 1",
-		Parent: &Parent{Firstname: "saurabh", Lastname: "raj"}})
-
-	students = append(students, Student{Roll: "2", Branch: "789456", Name: "Student 2",
-		Parent: &Parent{Firstname: "rahul", Lastname: "singh"}})
-
-	// Route Handler - will establish endpoints for our APIs
-	// route then the function to be executed when that route with given method is hit
 	r.HandleFunc("/api/students", getStudents).Methods("GET")
 	r.HandleFunc("/api/students/{roll}", getStudent).Methods("GET")
 	r.HandleFunc("/api/students", createStudent).Methods("POST")
